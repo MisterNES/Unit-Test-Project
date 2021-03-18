9252801f56314dcc276ddc6dc28864cd746b1de5
Nathan Darrow <nathanadarrow@gmail.com>
2021-03-18 15:25:42 -0700

    Project Complete

--- test/phase-3-spec.js
+++ test/phase-3-spec.js
@@ -6,48 +6,47 @@ describe('Word', function () {
   describe('Word constructor function', function () {
     it('should have a "word" property', function() {
       //Arrange & Act
+      let banana = new Word("banana");
 
-      
       //Assert
-       expect.fail('Remove this expect.fail and replace it with your test');
+       expect(banana).to.have.property("word");
     })
      it('should set the "word" property when a new word is created', function() {
       //Arrange & Act
+      let banana = new Word("banana");
 
-      
       //Assert
-        expect.fail('Remove this expect.fail and replace it with your test');
-    })
+      expect(banana.word).to.equal("banana");
+     })
   })
   describe('removeVowels function', function () {
     it('should return a the word with all vowels removed', function() {
       //Arrange
-      
+      let banana = new Word("banana");
       //Act
-      
+      let res = banana.removeVowels();
       //Assert
-       expect.fail('Remove this expect.fail and replace it with your test');
+       expect(res).to.equal("bnn");
     })
   })
   describe('removeConsonants function', function () {
 })
   it('should return the word with the consonants removed', function() {
-    //Arrange
-    
-    //Act
-    
-    //Assert
-     expect.fail('Remove this expect.fail and replace it with your test');
+      //Arrange
+      let banana = new Word("banana");
+      //Act
+      let res = banana.removeConsonants();
+      //Assert
+       expect(res).to.equal("aaa");
   })
   describe('pigLatin function', function () {
 })
   it('should return the word converted to pig latin', function() {
-    //Arrange
-    
-    //Act
-    
-    //Assert
-     expect.fail('Remove this expect.fail and replace it with your test');
-    
+      //Arrange
+      let banana = new Word("banana");
+      //Act
+      let res = banana.pigLatin();
+      //Assert
+       expect(res).to.equal("ananabay");
   })
 })
